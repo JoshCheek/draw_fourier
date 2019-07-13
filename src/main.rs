5f820d14299a67e4a837390da1ef0483e6df1c21
Josh Cheek <josh.cheek@gmail.com>
2019-07-13 14:45:09 -0500

    Perform the FFT on the SVG points

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate svg2polylines;
 use std::io::{self, Read};
 use std::process::exit;
-use svg2polylines::{Polyline};
+use svg2polylines::{CoordinatePair, Polyline};
+
+extern crate rustfft;
+use std::sync::Arc;
+use rustfft::FFTplanner;
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+
 
 fn main() {
     let mut buffer = String::new();
@@ -12,10 +19,25 @@ fn main() {
         exit(2);
     });
 
-    println!("Found {} polylines.", polylines.len());
+
     for line in polylines {
-        for coord in line {
-            println!("- {:?}", coord);
+        let num_samples = line.len();
+        let mut input:  Vec<Complex<f64>> = vec![Complex::zero(); num_samples];
+        let mut output: Vec<Complex<f64>> = vec![Complex::zero(); num_samples];
+
+        for (i, coord) in line.iter().enumerate() {
+            match coord {
+                CoordinatePair { x, y } => input[i] = Complex::new(*x, *y)
+            }
+        }
+
+        let mut planner = FFTplanner::new(false);
+        let fft = planner.plan_fft(num_samples);
+        fft.process(&mut input, &mut output);
+
+        for num in output {
+            println!("- {}", num);
         }
     }
+
 }
