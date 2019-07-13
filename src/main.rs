81c1f45187ede9ff61c496551d971c5d246ee76b
Josh Cheek <josh.cheek@gmail.com>
2019-07-13 13:53:09 -0500

    Reads the SVG off stdin

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ extern crate svg;
 use svg::node::element::path::{Command, Data};
 use svg::node::element::tag::Path;
 use svg::parser::Event;
+use std::io::{self};
+
 
 fn main() {
-
-    let path = "hydra.svg";
-    for event in svg::open(path).unwrap() {
+    for event in svg::read(io::stdin()).unwrap() {
         match event {
             Event::Tag(Path, _, attributes) => {
                 let data = attributes.get("d").unwrap();
