ccf6245ab56df229b19ae9ef40ef6d2ba99c6581
Josh Cheek <josh.cheek@gmail.com>
2019-07-13 13:44:03 -0500

    The SVG lib is able to open the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate svg;
+use svg::node::element::path::{Command, Data};
+use svg::node::element::tag::Path;
+use svg::parser::Event;
+
 fn main() {
-    println!("Hello, world!");
+
+    let path = "hydra.svg";
+    for event in svg::open(path).unwrap() {
+        match event {
+            Event::Tag(Path, _, attributes) => {
+                let data = attributes.get("d").unwrap();
+                let data = Data::parse(data).unwrap();
+                for command in data.iter() {
+                    match command {
+                        &Command::Move(..) => println!("Move!"),
+                        &Command::Line(..) => println!("Line!"),
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
 }
